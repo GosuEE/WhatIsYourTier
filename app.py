f6a522fdf5d0238911c8d6ca275cd0c7bf4c10cc
GosuEE <leemin727@naver.com>
2022-11-16 17:39:12 +0900

    수정 기능 추가

--- app.py
+++ app.py
@@ -161,29 +161,28 @@ def post_show():
         except jwt.exceptions.DecodeError:
             return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-# @app.route('/modify', methods=["GET","POST"])
-# def modify():
-#     if request.method == "POST":
-#         return render_template("modify.html")
-#     else:
-#         return render_template("modify.html")
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         user_info = db.user.find_one({"id": payload['id']})['id']
-#         title = request.form['title']
-#         contents = request.form['contents']
-#         num = request.form['num']
-#         print(db.board.find_one({"number": int(num)}))
-#         id = db.user.find_one({"id": payload['id']})['id']
-#         if id != db.user.find_one({"id": payload['id']})['id']:
-#             return redirect(url_for(f"/post/{num}", msg="본인의 글만 수정 가능합니다."))
-#         else:
-#             return render_template("modify.html", title=title, contents=contents)
-#     except jwt.ExpiredSignatureError:
-#         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-#     except jwt.exceptions.DecodeError:
-#         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+@app.route('/modify/<int:number>', methods=["GET"])
+def modify(number):
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})['id']
+        text = db.board.find_one({"number": int(number)})
+
+        title = text['title']
+        contents = text['contents']
+        id = db.user.find_one({"id": payload['id']})['id']
+
+        if id != db.user.find_one({"id": payload['id']})['id']:
+            return redirect(url_for(f"/post/{number}", msg="본인의 글만 수정 가능합니다."))
+        else:
+            print("else")
+            return render_template("modify.html", title=title, contents=contents)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    
 
 @app.route('/write', methods=["GET"])
 def write_get():
