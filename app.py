72f09656c0462f6e1bc7b8c6b49c548e179632ed
STEVE <kimhyosun324@naver.com>
2022-11-16 19:59:36 +0900

    메인 페이지

--- app.py
+++ app.py
@@ -1,148 +1,144 @@
 # -*- coding: utf-8 -*-
-
+import requests as requests
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+
 app = Flask(__name__)
-
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.xmayriq.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
-
 import certifi
+from urllib.request import urlopen
+from html_table_parser import parser_functions as parser
+from selenium import webdriver
 
-ca=certifi.where()
+ca = certifi.where()
 
-client = MongoClient("mongodb+srv://test:test@cluster0.15fhovx.mongodb.net/test", tlsCAFile=ca)
-db = client.dbsparta_plus_week4
-
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.xmayriq.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient("mongodb+srv://test:sparta@cluster0.xmayriq.mongodb.net/Cluster0?retryWrites=true&w=majority")
 db = client.dbsparta
 
-
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
 SECRET_KEY = 'SPARTA'
-
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
-import jwt
-
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
-import datetime
-
-# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
-# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
-import hashlib
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 
-#################################
-##  HTML을 주는 부분             ##
-#################################
-@app.route('/board')
-def home():
-    return render_template('index.1.html')
+# 코딩 시작
+# ------------네이버
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    nicname_receive = request.form['nicname_give']
+
+    url = 'https://fow.kr/find/' + nicname_receive
+
+    user_info_fow = requests.get(url, headers=headers)
+    user_info_fow = BeautifulSoup(user_info_fow.text, 'html.parser')
+    data = user_info_fow.find("table", {"class": "tablesorter"})
+    p = parser.make2d(data)
+    db.list.drop()
+    db.freeRank.drop()
+    db.soloRank.drop()
+    for x in range(len(p)):
+        champion = p[x][0]
+        cntgame = p[x][1]
+        Odds = p[x][2]
+        kda = p[x][3]
+        kill = p[x][4]
+        death = p[x][5]
+        assist = p[x][6]
+        cs = p[x][7]
+        avgold = p[x][8]
+        triple = p[x][9]
+        quadra = p[x][10]
+        penta = p[x][11]
+        win = p[x][12]
+        lose = p[x][13]
+        doc = {
+            'champion': champion,
+            'cntgame': cntgame,
+            'Odds': Odds,
+            'kda': kda,
+            'kill': kill,
+            'death': death,
+            'assist': assist,
+            'cs': cs,
+            'avgold': avgold,
+            'triple': triple,
+            'quadra': quadra,
+            'penta': penta,
+            'win': win,
+            'lose': lose
+        }
+
+        db.list.insert_one(doc)
+    #
+    #
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome(options=options)
+    # ---------------------------------------------------------------------------------------------
+
+    driver.get(url)
+    html = driver.page_source
+    soup_user_profile = BeautifulSoup(html, "html.parser")
+    items = soup_user_profile.select(".table_summary")
+
+    # 솔랭 정보 =====================================================================================
+    soloRank = str(items).split('<div style="width:5px; height:125px; position:relative; float:left;">')[0]
+    league = soloRank.split('리그: ')[1].split(' 5x5')[0]
+    ranking_num = soloRank.split('target="_blank"><b>')[1].split('</b><span')[0]
+    ranking_per = soloRank.split('위">( ')[1].split(' )</span>')[0]
+    tier = soloRank.split('<b><font color')[1].split('">')[1].split('</font>')[0]
+    point = soloRank.split('리그 포인트: ')[1].split('<br>')[0]
+    record = soloRank.split('</br></br></br></br></br></br></br></div>')[0].split('승급전:')[1].split('<br><br>')[1].split(
+        '			')[1].split('		')[0].replace("\n", "")
+    rankimg = soloRank.split(' src="')[1].split('" style')[0]
+    solo_doc = {
+        'name': nicname_receive,
+        'ranking_num': ranking_num,
+        'ranking_per': ranking_per,
+        'league': league,
+        'tier': tier,
+        'point': point,
+        'record': record,
+        'rankimg': rankimg
+    }
+    db.soloRank.insert_one(solo_doc)
+
+    # 자랭 정보 =====================================================================================
+    freeRank = str(items).split('<div style="width:5px; height:125px; position:relative; float:left;">')[1]
+    league = freeRank.split('리그: ')[1].split(' 5x5')[0]
+    ranking_num = freeRank.split('target="_blank"><b>')[1].split('</b><span')[0]
+    ranking_per = freeRank.split('위">( ')[1].split(' )</span>')[0]
+    tier = freeRank.split('<b><font color')[1].split('">')[1].split('</font>')[0]
+    point = freeRank.split('리그 포인트: ')[1].split('<br>')[0]
+    record = freeRank.split('</br></br></br></br></br></br></br></div>')[0].split('승급전:')[1].split('<br><br>')[1].split(
+        '			')[1].split('		')[0].replace("\n", "")
+    rankimg = freeRank.split(' src="')[1].split('" style')[0]
+    free_doc = {
+        'ranking_num': ranking_num,
+        'ranking_per': ranking_per,
+        'league': league,
+        'tier': tier,
+        'point': point,
+        'record': record,
+        'rankimg': rankimg
+    }
+    db.freeRank.insert_one(free_doc)
+    return jsonify({'msg': '입력 완료!'})
+
+
+@app.route("/user_info", methods=["GET"])
+def homework_get():
+    soloRank_list = list(db.soloRank.find({}, {'_id': False}))
+    freeRank_list = list(db.freeRank.find({}, {'_id': False}))
+    champion_list = list(db.list.find({}, {'_id': False}))
+    return jsonify({'soloRank': soloRank_list, 'freeRank': freeRank_list, 'champion': champion_list})
+
 
 @app.route('/')
 def home():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-        return render_template('index.html', nickname=user_info["nick"])
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
-
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-
-#################################
-##  로그인을 위한 API            ##
-#################################
-
-# [회원가입 API]
-# id, pw, nickname을 받아서, mongoDB에 저장합니다.
-# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
-@app.route('/api/register', methods=['POST'])
-def api_register():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    nickname_receive = request.form['nickname_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-
-    return jsonify({'result': 'success'})
-
-
-# [로그인 API]
-# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
-@app.route('/api/login', methods=['POST'])
-def api_login():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
-
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
-    if result is not None:
-        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
-        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
-        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
-        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
-        payload = {
-            'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        # token을 줍니다.
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
-
-# [유저 정보 확인 API]
-# 로그인된 유저만 call 할 수 있는 API입니다.
-# 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
-# (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
-@app.route('/api/nick', methods=['GET'])
-def api_valid():
-    token_receive = request.cookies.get('mytoken')
-
-    # try / catch 문?
-    # try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
-
-    try:
-        # token을 시크릿키로 디코딩합니다.
-        # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        print(payload)
-
-        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
-        # 여기에선 그 예로 닉네임을 보내주겠습니다.
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
-        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
-    except jwt.ExpiredSignatureError:
-        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
-        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
-    except jwt.exceptions.DecodeError:
-        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
