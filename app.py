164b5b53ada15b67b2f55d7ba0458cecad496aa3
GosuEE <leemin727@naver.com>
2022-11-16 09:13:44 +0900

    기능 추가

--- app.py
+++ app.py
@@ -144,8 +144,7 @@ def post(number):
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-
+        
 @app.route('/post', methods=["GET","POST"])
 def post_show():
         token_receive = request.cookies.get('mytoken')
